65e8f01938146bcecf0f6a1c264903c2858652fa
Torry <torry_tong@outlook.com>
2023-05-01 14:30:22 -0700

    test

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -8,9 +8,11 @@ Set::Set(){
 
 Set::Set(const Set& other):mRoot(nullptr){
     if (other.mRoot == nullptr) {
+        std::cout << "running2" << std::endl;
         return;
     }
     else if (other.mRoot->leftchild == nullptr && other.mRoot->rightchild == nullptr) {
+        std::cout << "running3" << std::endl;
         this->mRoot = new Node(other.mRoot->element);
     }
     else {
