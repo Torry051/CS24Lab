9fedb7e67be3377b5efc3a1e403e911a86573835
Torry <torry_tong@outlook.com>
2023-05-17 21:41:07 -0700

    test

--- counter/Index.cpp
+++ counter/Index.cpp
@@ -23,9 +23,10 @@ Index::Index(size_t val){
 
 size_t Index::idx(std::string str) const{
     size_t result=0;
-    for (size_t i= 0;i<str.length(); i++){
-        result += str[i];
-    }
+    result = str[0];
+    // for (size_t i= 0;i<str.length(); i++){
+    //     result += str[i];
+    // }
     result = result % (capacity);
     return result;
 }
