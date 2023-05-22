b49035bb4f1ac246ecc0118b60346208bfaeb9fb
Torry <torry_tong@outlook.com>
2023-05-22 14:03:29 -0700

    test

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -24,10 +24,10 @@ Heap::Heap(const Heap& other){
 }
 
 Heap::Heap(Heap&& other){
-    // mData = other.mData;
-    // this->mCapacity = other.mCapacity;
-    // this->mCount = other.mCount;
-    this = other;
+    mData = other.mData;
+    other.mData = nullptr;
+    this->mCapacity = other.mCapacity;
+    this->mCount = other.mCount;
 }
 
 Heap::~Heap(){
