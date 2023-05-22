64ca1fa93f82509b4fd423bf92c623ee8e64605c
Torry <torry_tong@outlook.com>
2023-05-21 22:11:23 -0700

    test

---
+++ typo/WordList.cpp
@@ -0,0 +1,52 @@
+#include "WordList.h"
+
+WordList::WordList(std::istream& stream){
+    std::string line;
+    while(std::getline(stream,line)){
+        bool lower = true;
+        if (line[0]=='#'){
+            continue;
+        }
+        for(size_t i = 0; i<line.length();i++){
+            if (!(islower(line[i]))){
+                lower = false;
+                break;
+            }
+        }
+        if(lower){
+            mWords.push_back(line);
+        }
+    }
+}
+
+Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const{
+    Heap heap(mWords.size());
+    std::string str;
+    size_t index = 0;
+
+    for (size_t i = 0; i < mWords.size(); i++){
+        str = mWords.at(i);
+        float scores = 0;
+        for (size_t j = 0; j < str.length(); j++){
+            float d = 0;
+            d = (points.at(index).x - QWERTY[str[j]].x) * (points.at(index).x - QWERTY[str[j]].x) + (points.at(index).y - QWERTY[str[j]].y) * (points.at(index).y - QWERTY[str[j]].y);
+            scores += 1/(10*d*d+1);
+            index = index + 1;
+        }
+        scores = scores/(str.length());
+        heap.push(str,scores);
+    }
+
+    while(heap.count()>maxcount){
+        heap.pop(); 
+    }
+    Heap result(maxcount);
+    for(size_t z = 0; z < maxcount; z++){
+        Heap::Entry E = heap.pop();
+        result.push(E.value,E.score);
+    }
+    for (size_t i =0; i< cutoff; i++){
+        Heap::Entry E = result.pop();
+    }
+    return result;
+}
--- typo/test.cpp
+++ typo/test.cpp
@@ -1,4 +1,4 @@
-#include "Heap.h"
+// #include "Heap.h"
 #include <iostream>
 #include <vector>
 
@@ -7,24 +7,28 @@
 // This file won't be graded - do whatever you want.
 
 int main() {
-    Heap heap(8);
-    // heap.lookup(8);
-    heap.push("torry", 0.99);
-    heap.push("rai", 0.78);
-    heap.push("xer", 0.85);
-    heap.push("t",0.79);
-    // Heap heap2(&heap);
-    //  Heap::Entry test = heap.pop();
-    Heap::Entry test = heap.pushpop("rasss",0.8);
-    for (size_t i = 0; i< 8; i++){
-        // std::cout << "running" <<std::endl;
-        std::cout << heap.mData[i].value << " " << heap.mData[i].score <<std::endl;
-    }
-    std::cout << heap.count() <<std::endl;
+    // Heap heap(8);
+    // // heap.lookup(8);
+    // heap.push("torry", 0.99);
+    // heap.push("rai", 0.78);
+    // heap.push("xer", 0.85);
+    // heap.push("t",0.79);
+    // // Heap heap2(&heap);
+    // //  Heap::Entry test = heap.pop();
+    // Heap::Entry test = heap.pushpop("rasss",0.8);
+    // for (size_t i = 0; i< 8; i++){
+    //     // std::cout << "running" <<std::endl;
+    //     std::cout << heap.mData[i].value << " " << heap.mData[i].score <<std::endl;
+    // }
+    // std::cout << heap.count() <<std::endl;
 
 
-    // std::cout << "This program doesn't do anything interesting yet...\n";
+    // // std::cout << "This program doesn't do anything interesting yet...\n";
 
-
-    return 0;
+    int * num = new int[8];
+    int * num1;
+    num[0]= 2;
+    num1 = num;
+    std::cout << num1[1] <<std::endl;
+    // return 0;
 }
