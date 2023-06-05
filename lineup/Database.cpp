ea3983be21521ff31d7ab0c6d942e3c7ed18ff35
Torry <torry_tong@outlook.com>
2023-06-05 10:08:58 -0700

    test

--- lineup/Database.cpp
+++ lineup/Database.cpp
@@ -2,6 +2,62 @@
 
 // Database Member Functions
 
-Database* Database::create() {
-  return new Database();
+Database *Database::create()
+{
+	return new Database();
+}
+Database::Database()
+{
+	;
+}
+
+void Database::insert(const Report *report)
+{
+	if (this->data.count(report->id) == 0)
+	{
+		this->data[report->id] = report;
+	}
+	else
+	{
+		throw DuplicateReport(report->id);
+	}
+}
+
+Database::~Database()
+{
+	;
+}
+
+std::vector<const Report *> Database::search(float age, float height, float weight) const
+{
+	std::vector<const Report *> result;
+	for (auto pair = this->data.begin(); pair != this->data.end(); pair++)
+	{
+		if (age < pair->second->age.min || age > pair->second->age.max)
+		{
+			continue;
+		}
+		if (height < pair->second->height.min || age > pair->second->height.max)
+		{
+			continue;
+		}
+		if (weight < pair->second->weight.min || age > pair->second->weight.max)
+		{
+			continue;
+		}
+		result.push_back(pair->second);
+	}
+	return result;
+}
+
+void Database::remove(unsigned int id)
+{
+	if (this->data.count(id) == 0)
+	{
+		throw NoSuchReport(id);
+	}
+	else
+	{
+		this->data.erase(id);
+	}
 }
--- lineup/Database.h
+++ lineup/Database.h
@@ -2,23 +2,29 @@
 #define DATABASE_H
 
 #include <vector>
+#include <list>
+
+#include <unordered_map>
 
 #include "Report.h"
+#include "Errors.h"
 
-class Database {
+class Database
+{
   // Member Variables
+  std::unordered_map<unsigned int, const Report *> data;
 
   // Helper Functions
 
 public:
-  static Database* create();
+  static Database *create();
 
 public:
   Database();
   ~Database();
 
-  void insert(const Report* report);
-  std::vector<const Report*> search(float age, float height, float weight) const;
+  void insert(const Report *report);
+  std::vector<const Report *> search(float age, float height, float weight) const;
   void remove(unsigned int id);
 };
 
